a562c89598ec99c4f6040b50df787d4841222aad
Simodeme2 <Simodeme2@student.arteveldehs.be>
2017-10-05 10:57:08 +0200

    Update Git Reponsitory

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Oefening 1</title>
+</head>
+
+<body>
+<?php
+    $x = 10;
+    $y = 7;
+
+    echo $x + $y;
+    echo "<br>";
+    echo $x - $y;
+    echo "<br>";
+    echo $x * $y;
+    echo "<br>";
+    echo $x / $y;
+    echo "<br>";
+    echo $x % $y;
+?>
+</body>
+</html>
